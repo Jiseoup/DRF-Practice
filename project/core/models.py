d4ee261b02ef97b3311a1fb800f718fdac9dc9e4
Jiseoup <jseoup@gmail.com>
2025-04-15 20:42:10 +0900

    Modify Models

--- project/core/models.py
+++ project/core/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     post_no = models.AutoField(primary_key=True)
-    author = models.ForeignKey(User, models.PROTECT)
+    author = models.ForeignKey(User, models.PROTECT, db_column='author')
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -17,8 +17,8 @@ class Post(models.Model):
 
 class Comment(models.Model):
     comment_no = models.BigAutoField(primary_key=True)
-    post = models.ForeignKey(Post, models.PROTECT)
-    author = models.ForeignKey(User, models.PROTECT)
+    post = models.ForeignKey(Post, models.PROTECT, db_column='post')
+    author = models.ForeignKey(User, models.PROTECT, db_column='author')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
